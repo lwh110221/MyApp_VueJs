90c737a48bd4e38ae6fbeac407f826c1efddca72
lwh110221 <2285813721@qq.com>
2025-01-02 20:15:37 +0800

    chore: 开发模式中加入vue-devtools

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import vueDevtools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevtools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 5173
   }
 })
